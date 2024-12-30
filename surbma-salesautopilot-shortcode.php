954cf470a7d3a1a9c92fc3354aacead221fb6718
Surbma <peter@surbma.hu>
2024-12-30 12:52:25 +0100

    2.5
    - Nothing changed, but need to increase the version.

--- surbma-salesautopilot-shortcode.php
+++ surbma-salesautopilot-shortcode.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma | SalesAutopilot Shortcode
 Plugin URI: https://surbma.com/wordpress-plugins/
 Description: A simple shortcode to include SalesAutopilot forms into WordPress.
 
-Version: 2.4
+Version: 2.5
 
 Author: Surbma
 Author URI: https://surbma.com/
